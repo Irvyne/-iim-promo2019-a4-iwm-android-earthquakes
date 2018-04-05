cc64fc23f5d9a2eced6de703cb8ec70ea45f49d6
Thibaud BARDIN <thibaud.bardin@gmail.com>
2018-04-05 17:18:08 +0200

    😔

--- app/src/main/java/ninja/irvyne/iwma4earthquakes/EarthquakeAdapter.kt
+++ app/src/main/java/ninja/irvyne/iwma4earthquakes/EarthquakeAdapter.kt
@@ -2,6 +2,7 @@ package ninja.irvyne.iwma4earthquakes
 
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import ninja.irvyne.iwma4earthquakes.api.model.Feature
@@ -13,22 +14,24 @@ class EarthquakeAdapter(private val myDataset: ArrayList<Feature>) :
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder.
     // Each data item is just a string in this case that is shown in a TextView.
-    inner class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        val magnitude: TextView = view.findViewById(R.id.earthquakeMagnitude)
+        val title: TextView = view.findViewById(R.id.earthquakeTitle)
+        val snippet: TextView = view.findViewById(R.id.earthquakeSnippet)
+    }
 
     // Create new views (invoked by the layout manager)
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EarthquakeAdapter.ViewHolder {
-        // create a new view
-        val textView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_earthquake, parent, false) as TextView
-        // set the view's size, margins, paddings and layout parameters
-
-        return ViewHolder(textView)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EarthquakeAdapter.ViewHolder = ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_earthquake, parent, false)
+    )
 
     // Replace the contents of a view (invoked by the layout manager)
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val feature = myDataset[position]
 
-        holder.textView.text = feature.properties?.place
+        holder.magnitude.text = "%.1f".format(feature.properties?.mag)
+        holder.title.text = feature.properties?.place
+        holder.snippet.text = feature.properties?.title
     }
 
     // Return the size of your dataset (invoked by the layout manager)
